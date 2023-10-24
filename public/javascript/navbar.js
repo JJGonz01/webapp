c97009cd917441f3947dd70cb8c77459f872ca9b
JoseJesus <josejesus.gonzalez@uclm.es>
2023-10-24 19:05:48 +0200

    navbar

--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -2,14 +2,14 @@
 
 function navbarStart() {
     const navContainer = document.getElementById("vertical_nv");
-        if(!localStorage["navbar"] || localStorage["navbar"]!="true"){//se abre
-            localStorage["navbar"] = "false";
-            navContainer.classList.remove("navbar-vertical");
-            navContainer.classList.add("navbar-vertical-closed");
-        }
-        else{
+        if(!localStorage["navbar"] || localStorage["navbar"]!="false"){//se abre
+            localStorage["navbar"] = "true";
             navContainer.classList.remove("navbar-vertical-closed");
             navContainer.classList.add("navbar-vertical");
+        }
+        else{
+            navContainer.classList.remove("navbar-vertical");
+            navContainer.classList.add("navbar-vertical-closed");
         }
 }  
 
