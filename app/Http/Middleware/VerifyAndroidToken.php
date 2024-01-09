62e123939f2a421531521e49fdecad3fe084fcda
Jose Jesus <146087300+JJGonz01@users.noreply.github.com>
2024-01-09 15:12:58 +0100

    [JJGG] Build: TOKEN

---
+++ app/Http/Middleware/VerifyAndroidToken.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use \Firebase\JWT\JWT; 
+use Firebase\JWT\Key;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+
+
+class VerifyAndroidToken extends Middleware{
+
+    public function handle($request, Closure $next, ...$guards)
+    {
+        $guard = [
+            '/session/response',
+            '/start',
+            '/finish',
+            '/finishExtra',
+            '/getrules'
+        ];
+    
+        $token = $request->header('Authorization');
+        if(!in_array($request -> path(), $guard)){ /*No es una ruta a la que haya que aplicar el middleware*/
+            return $next($request);
+        }
+
+        if (!$token) {//modificar
+            return response()->json(['error' => 'Token no proporcionado'], 401);
+        }
+
+        $secretKey = env('ANDROID_TOKEN_KEY', '');
+
+        $secret = base64_decode($secretKey);
+        
+        try {
+            $decoded = JWT::decode($token, new Key($secret, 'HS256'));
+            return $next($request);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Token inválidso',$e->getMessage()], 401);
+        }
+    }
+
+}
