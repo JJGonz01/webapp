13fe35626465d8527cde840b20249524b4f2d079
Jose Jesus <josejesus.gonzalez@uclm.es>
2024-02-19 20:45:06 +0100

    [JJGG[ Build: create patient start

--- resources/views/patients/create_patient.blade.php
+++ resources/views/patients/create_patient.blade.php
@@ -5,9 +5,41 @@
 
 <head>
     <title>CREAR PACIENTE</title>
+    <link rel="stylesheet" href="{{asset('/css/dashboards/patients/create-patient.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/dashboards/patients.css')}}">
 </head>
 
-<div>
+<!-- Popup -->
+
+<div id="popup" class="popup">
+    <div class="popup-content">
+        <form action="{{route('patients_create', [], false, true)}}" id= 'form_pat' method="POST">
+            @csrf
+            @if (session('success'))
+                <h6 class="alert alert-success"> {{ session('success') }}</h6>
+            @endif
+
+            @error('name')
+                <h6 class="alert alert-danger"> Falta nombre: {{ $message }}</h6>
+            @enderror
+
+            @error('surname')
+                <h6 class="alert alert-danger"> Falta apellido: {{ $message }}</h6>
+            @enderror
+            <h2>Añadir paciente</h2>
+            <p>Imagen</p>
+            <img id="profile-image" src="{{asset('images/perfil.png')}}"></img>
+
+            <input placeholder = "Nombre del paciente" type="text" name = "name" id = "name" class = "form-control">
+
+            <input type="text" placeholder = "Apellidos" name = "surname" id = "surname" class = "form-control">
+
+            <textarea type="text" placeholder = "Comentario/Descripción" name = "description" id="commentary" class = "form-control" rown="10"></textarea>
+            <button id="go_to_patient_create" type="submit"> CREAR PACIENTE </button>
+        </form>
+    </div>
+</div>
+<!--div>
 
     <form action="{{route('patients_create', [], false, true)}}" id= 'form_pat' method="POST">
     @csrf
@@ -37,8 +69,8 @@
         <p>COMENTARIO</p>
         <textarea type="text" name = "description" id="commentary" class = "form-control" rown="10"></textarea>
         <button id="go_to_patient_create" type="submit"> CREAR PACIENTE </button>
-    </form> <!--FORM PACIENTE-->
+    </form>
     </div>
     </div>
-</div>
+</div -->
 @endsection
